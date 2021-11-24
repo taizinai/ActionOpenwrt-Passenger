ca86ce18763ade1a31b2f162d963bd4169ff7e4e
passengerya <yangrunzeng@163.com>
2021-11-24 20:00:14 +0800

    Create diy-part1.sh

---
+++ scripts/latest_ua2f/diy-part1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+
+# UA2F相关插件
+git clone -b 20210531T234622 https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+
+# 自定义添加插件
+git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
