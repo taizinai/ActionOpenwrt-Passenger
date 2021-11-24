700f3075857e3225c05ff0fb0f28ff29ff65af49
passengerya <yangrunzeng@163.com>
2021-11-24 10:21:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题为argon_new
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' feeds/luci/collections/luci/Makefile
 
 # 固件版本栏自定义用户名(好像改不了)
 # sed -i "s/OpenWrt /passenger compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
