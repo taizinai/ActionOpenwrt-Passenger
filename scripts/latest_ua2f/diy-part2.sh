983a87afe22ddcab2f35504bd0c3d605615e336a
passengerya <yangrunzeng@163.com>
2021-11-29 21:31:20 +0800

    Update diy-part2.sh

--- scripts/latest_ua2f/diy-part2.sh
+++ scripts/latest_ua2f/diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # 修改默认主题为argon_new
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' ./feeds/luci/collections/luci/Makefile
 
 # 固件版本栏自定义用户名(好像改不了)
 # sed -i "s/OpenWrt /passenger compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
