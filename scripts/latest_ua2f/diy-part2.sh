646b83aa5a8468f43a4f47504071399333ca375e
passengerya <yangrunzeng@163.com>
2021-11-29 21:29:47 +0800

    Update diy-part2.sh

--- scripts/latest_ua2f/diy-part2.sh
+++ scripts/latest_ua2f/diy-part2.sh
@@ -13,6 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
+# 取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # 修改默认主题为argon_new
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' feeds/luci/collections/luci/Makefile
 
